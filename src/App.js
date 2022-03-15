46af9459504309ec8184bb23ba630a23e48ed1a5
Rasujon3 <rasujon3@gmail.com>
2022-03-15 23:41:01 +0600

    45-3 Create Component, return HTML from a component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 const number = 5555;
-const singer = {name: 'Dr. Mahfuz',age: 50,job:'singer'}
-const singer2 = {name: 'Eva Rahman',age: 50,job:'singer2'}
+const singer = { name: "Dr. Mahfuz", age: 50, job: "singer" };
+const singer2 = { name: "Eva Rahman", age: 50, job: "singer2" };
 
 const singerStyle = {
-  color: 'purple',
-  backgroundColor: 'white'
-}
+  color: "purple",
+  backgroundColor: "white",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello dude, How are you?</h3>
-        </div>
-        <div className="music">
-          <p>Name: {2222+number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job} </p>
-          <p style={{color:'blue',backgroundColor:'yellow'}} >Name: {singer2.name} {singer2.job} </p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person />
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New component. yehhhh</h5>
+      <p>Rock mama react mama.</p>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+    </div>
+  );
+}
+
+const Person = () => {
+  return (
+    <div className="person">
+      <h1>Sakib Al Hasan</h1> <p>Profession: Cricketer</p>
+    </div>
+  );
+};
+
+function Friend() {
+  return (
+    <div className="container">
+      <h3>Name: AJy Devgan</h3>
+      <p>job: maramari</p>
     </div>
   );
 }
